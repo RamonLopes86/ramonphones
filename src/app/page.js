72cb9f06f9fed80cefc0a225411bf87d79625df1
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2025-01-03 14:11:11 -0300

    ajuste de api ml

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
     <ContextProvaider>
 
 
-      <div>
+      <div style={{overflow:'hidden'}}>
 
 
           <TelaLogin/>
--- src/components/sectionItensML/itensML.jsx
+++ src/components/sectionItensML/itensML.jsx
@@ -2,15 +2,20 @@
 import hookcontext from '@/contexto/hookcontext';
 import estiloML from './estiloML.module.css';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { FaCartPlus } from "react-icons/fa6";
 import dinheiro from '@/funcoes/funcoes';
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 
 export default function ProdutosMl() {
 
     const { inputPesquisa, arrayML, setArrayML } = hookcontext()
+    const [mostrarCard , setMostrarCard] = useState(false)
+    const [loading , setLoading] = useState(true)
+    const [msg , setMsg] = useState('')
+    const [mostraCircle , setMostrarCircle] = useState(false)
 
 
 
@@ -19,11 +24,26 @@ export default function ProdutosMl() {
 
         try {
 
-            const response = await axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${inputPesquisa}`)
+        const response = await axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${inputPesquisa}`)
 
-            const { results } = response.data
+        const { results } = response.data
 
-            return setArrayML(results)
+        if(results.length === 0){
+
+            setLoading(true)
+            
+            setMsg('nenhum iten encontrado :(')
+            setMostrarCircle(false)
+
+            return;
+
+        }
+            
+
+        setArrayML(results)
+        setLoading(false)
+
+        return;
 
         } catch (error) {
 
@@ -35,9 +55,32 @@ export default function ProdutosMl() {
 
 
 
+
     useEffect(() => {
 
-        exibirProdutosMl()
+        if(inputPesquisa && inputPesquisa.length > 1 && /^(?!\s*$).+/.test(inputPesquisa) && inputPesquisa !== " " ){
+
+            setMostrarCard(true)
+
+        }else{
+
+            setMostrarCard(false)
+           
+            
+        }
+
+        setLoading(true) 
+        setMsg('carregando itens...')
+        setMostrarCircle(true)
+        
+        const timeout = setTimeout(()=>{exibirProdutosMl()},1400)
+
+        return ()=>{
+
+            clearTimeout(timeout)
+
+        }
+
 
     }, [inputPesquisa])
 
@@ -46,42 +89,57 @@ export default function ProdutosMl() {
 
     return (
 
-        <section className={estiloML.boxML}>
+    
+
+            <section className={`${estiloML.boxML} ${mostrarCard ? estiloML.cardOn : estiloML.cardOff}`}>
 
 
 
             <div className={estiloML.cardMl}>
 
                 {
+                    
+                    loading ? (
 
-                    arrayML.map((itens) => {
+                        <div  style={inputPesquisa.length === 0 ? {display:'none'} : {display:'block'} }  className={estiloML.boxLoading}>
+                            <h1 className={estiloML.msgCarrega}>{msg}</h1>
+                            <AiOutlineLoading3Quarters style={mostraCircle ? {opacity:'1'} : {opacity:'0'}} className={estiloML.iconCircle} />
+                        </div>
 
-                        {console.log(itens)}
+                    ):(
 
-                        return (
+                        arrayML.map((itens) => {
 
-                            <div key={itens.id} className={estiloML.card}>
+                        
 
-
-                                <div className={estiloML.conteudo}>
-
-                                    <Image className={estiloML.imgProd} alt={itens.title} width={100} height={100} quality={100} src={itens.thumbnail} />
-
-                                    <div className={estiloML.boxInfo}>
-                                        <h4>{itens.title}</h4>
-                                        <p>{dinheiro(itens.price , 'BRL')}</p>
+                            return (
+    
+                                <div key={itens.id} className={estiloML.card}>
+    
+    
+                                    <div className={estiloML.conteudo}>
+    
+                                        <Image className={estiloML.imgProd} alt={itens.title} width={100} height={100} quality={100} src={itens.thumbnail} />
+    
+                                        <div className={estiloML.boxInfo}>
+                                            <h4>{itens.title}</h4>
+                                            <p>{dinheiro(itens.price , 'BRL')}</p>
+                                        </div>
+    
                                     </div>
-
+    
+    
+                                    <FaCartPlus className={estiloML.iconCar} />
                                 </div>
+    
+                            )
+    
+    
+                        })
 
+                    )
 
-                                <FaCartPlus className={estiloML.iconCar} />
-                            </div>
-
-                        )
-
-
-                    })
+                  
 
 
                 }
@@ -90,6 +148,12 @@ export default function ProdutosMl() {
 
 
         </section>
+
+           
+
+    
+
+   
 
 
     )
